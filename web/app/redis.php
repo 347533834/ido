b3ad5ca83ed41950e0ffe05be75ca6ba0c91287a
347533834 <347533834@qq.com>
2025-12-19 00:21:56 +0800

    11

--- web/app/redis.php
+++ web/app/redis.php
@@ -4,7 +4,7 @@ return array(
     //'host' => '172.31.99.3',
     'host' => '127.0.0.1',
     'port' => 6379,
-    'password' => 'qq420984',
+    //'password' => 'qq420984',
     'select' => 0,
     'timeout' => 0,
     'expire' => 0,
