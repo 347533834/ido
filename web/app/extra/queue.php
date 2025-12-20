c13cc279268a77c3207c37ae2128b9296e256faf
347533834 <347533834@qq.com>
2025-12-20 17:18:00 +0800

    11

--- web/app/extra/queue.php
+++ web/app/extra/queue.php
@@ -5,7 +5,7 @@ return [
     'default' => 'default',       // 默认的队列名称
     'host' => '127.0.0.1',        // redis 主机ip
     'port' => 6379,               // redis 端口
-    //'password' => 'qq420984',             // redis 密码
+    'password' => 'qq420984',             // redis 密码
     'select' => 0,                // 使用哪一个 db，默认为 db0
     'timeout' => 0,               // redis连接的超时时间
     'persistent' => false,        // 是否是长连接
